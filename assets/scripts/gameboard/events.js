c8049e48bb4a4efccbb09f3ad903c75082ae9ed6
torystosse <tory.mae.stosse@gmail.com>
2019-11-15 10:29:22 -0500

    Added messaging for winner
    Fixed bug of adding messaging for winner, and it now replaces the next
    move

--- assets/scripts/gameboard/events.js
+++ assets/scripts/gameboard/events.js
@@ -12,8 +12,10 @@ let currentPlayer = 'o'
 const switchPlayer = () => {
   if (currentPlayer === 'o') {
     currentPlayer = 'x'
+    $('.message').text("Player O's move")
   } else {
     currentPlayer = 'o'
+    $('.message').text("Player X's move")
   }
 }
 // below will replace the index with the value
@@ -30,31 +32,21 @@ const onMakeMove = event => {
     $(event.target).text('x')
     // assigns id based on the ID of the div in HTML
     const id = event.target.id
-    console.log(id)
+    // console.log(id)
     console.log(gameLogic.gameBoard)
     moveToGameBoard(id, currentPlayer)
-    if (gameLogic.checkForWinner() === true) {
-      $('span').text('player x wins!')
-    }
-    // if (gameLogic.checkForWinner() === true) {
-    //   $('span').text('player x wins!')
-    // }
-    $('span').text("Player O's move")
+    gameLogic.checkForWinner()
     switchPlayer()
   } else if ((currentPlayer === 'o') && ($(event.target).html() === '')) {
     $(event.target).text('o')
     const id = event.target.id
-    console.log(id)
+    // console.log(id)
     console.log(gameLogic.gameBoard)
     moveToGameBoard(id, currentPlayer)
     gameLogic.checkForWinner()
-    // if (gameLogic.checkForWinner() === true) {
-    //   $('span').text('player o wins!')
-    // }
-    $('span').text("Player X's move")
     switchPlayer()
   } else {
-    $('span').text('You clicked this box already.')
+    $('.message').text('You clicked this box already.')
   }
 }
 
--- assets/scripts/gameboard/win-lose-logic.js
+++ assets/scripts/gameboard/win-lose-logic.js
@@ -1,40 +1,58 @@
 'use strict'
 
+// const moveEvents = require('')
+
 const gameBoard = ['', '', '', '', '', '', '', '', '']
 
 const checkForWinner = () => {
   if (gameBoard[0] === 'x' && gameBoard[1] === 'x' && gameBoard[2] === 'x') {
     console.log('Player X wins!')
+    $('.message').replaceWith('Player X wins!')
   } else if (gameBoard[0] === 'x' && gameBoard[4] === 'x' && gameBoard[8] === 'x') {
     console.log('Player X wins!')
+    $('.message').replaceWith('Player X wins!')
   } else if (gameBoard[0] === 'x' && gameBoard[3] === 'x' && gameBoard[6] === 'x') {
     console.log('Player X wins!')
+    $('.message').replaceWith('Player X wins!')
   } else if (gameBoard[1] === 'x' && gameBoard[4] === 'x' && gameBoard[7] === 'x') {
     console.log('Player X wins!')
+    $('.message').replaceWith('Player X wins!')
   } else if (gameBoard[2] === 'x' && gameBoard[4] === 'x' && gameBoard[6] === 'x') {
     console.log('Player X wins!')
+    $('.message').replaceWith('Player X wins!')
   } else if (gameBoard[2] === 'x' && gameBoard[5] === 'x' && gameBoard[8] === 'x') {
     console.log('Player X wins!')
+    $('.message').replaceWith('Player X wins!')
   } else if (gameBoard[3] === 'x' && gameBoard[4] === 'x' && gameBoard[5] === 'x') {
     console.log('Player X wins!')
+    $('.message').replaceWith('Player X wins!')
   } else if (gameBoard[6] === 'x' && gameBoard[7] === 'x' && gameBoard[8] === 'x') {
     console.log('Player X wins!')
+    $('.message').replaceWith('Player X wins!')
   } else if (gameBoard[0] === 'o' && gameBoard[1] === 'o' && gameBoard[2] === 'o') {
     console.log('Player O wins!')
+    $('.message').replaceWith('Player O wins!')
   } else if (gameBoard[0] === 'o' && gameBoard[4] === 'o' && gameBoard[8] === 'o') {
     console.log('Player O wins!')
+    $('.message').replaceWith('Player O wins!')
   } else if (gameBoard[0] === 'o' && gameBoard[3] === 'o' && gameBoard[6] === 'o') {
     console.log('Player O wins!')
+    $('.message').replaceWith('Player O wins!')
   } else if (gameBoard[1] === 'o' && gameBoard[4] === 'o' && gameBoard[7] === 'o') {
     console.log('Player O wins!')
+    $('.message').replaceWith('Player O wins!')
   } else if (gameBoard[2] === 'o' && gameBoard[4] === 'o' && gameBoard[6] === 'o') {
     console.log('Player O wins!')
+    $('.message').replaceWith('Player O wins!')
   } else if (gameBoard[2] === 'o' && gameBoard[5] === 'o' && gameBoard[8] === 'o') {
     console.log('Player O wins!')
+    $('.message').replaceWith('Player O wins!')
   } else if (gameBoard[3] === 'o' && gameBoard[4] === 'o' && gameBoard[5] === 'o') {
     console.log('Player O wins!')
+    $('.message').replaceWith('Player O wins!')
   } else if (gameBoard[6] === 'o' && gameBoard[7] === 'o' && gameBoard[8] === 'o') {
     console.log('Player O wins!')
+    $('.message').replaceWith('Player O wins!')
   }
 }
 
